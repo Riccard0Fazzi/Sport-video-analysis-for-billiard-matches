5769b3dde0d12623c3fdd0b81a31508cd3adabe2
FazziTux <69319073+FazziTux@users.noreply.github.com>
2024-07-04 13:01:22 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "../include/field_detection.h"
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <video_path>" << std::endl;
+        return -1;
+    }
+
+    cv::VideoCapture cap(argv[1]);
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Could not open video file." << std::endl;
+        return -1;
+    }
+	
+	// vector that stores each video frame
+	std::vector<cv::Mat> video_frames;
+	// temporary object to store each single frame
+	cv::Mat frame;
+
+	// Read and store frames until video is complete
+    while (cap.read(frame)) {
+        video_frames.push_back(frame.clone()); // Use clone to copy the frame into vector
+    }
+    // Release the video capture object
+    cap.release();
+	cv::Mat test = detectField(video_frames[0]);
+	std::cout << "DEBUG" << std::endl;	
+	cv::namedWindow("Test_Frames");
+	cv::imshow("Test_Frames",test);
+	cv::waitKey(0);
+	/* Show all video frames
+	cv::namedWindow("Test_Frames");
+	int i = 0;
+	while(i < video_frames.size())
+	{
+		cv::imshow("Test_Frames",video_frames[i]);
+		i++;
+		cv::waitKey(0);
+	}
+    cv::destroyAllWindows();
+*/
+
+    cv::destroyAllWindows();
+    return 0;
+}
+
